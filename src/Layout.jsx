dea5decb7a5f31ddb1b1159f2c324104c84f09e4
AbiodunOlaoluwa <mate.olaoluwa@gmail.com>
2024-06-04 01:30:16 +0100

    Did some cosmetic work on the layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,7 +6,7 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div>
+    <div className="layoutContainer">
       <div className={`blobContainer`}>
         <img src={blob1} className="blob" alt="blob" />
         <img src={blob2} className="blob" alt="blob" />
