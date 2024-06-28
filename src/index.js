0464cd3a6ccbb0f9c5c7ff8eb31a169c938f702c
AbiodunOlaoluwa <mate.olaoluwa@gmail.com>
2024-06-28 01:27:12 +0100

    Commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import axios from 'axios';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
